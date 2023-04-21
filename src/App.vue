c1cb847e91ead21c47192be67e00c0ca7771fce3
Gim128 <gimhanpabasara4@gmail.com>
2023-04-21 17:15:56 +0530

    To-do List App using Vue.js

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <home-component />
+</template>
+
+<script>
+  import {defineAsyncComponent} from 'vue'
+
+  // import HomeComponent from './components/HomeComponent.vue';
+
+  const  HomeComponent = defineAsyncComponent(()=> import('./components/HomeComponent.vue'));
+  
+  export default {
+    components:{
+      HomeComponent
+    }
+  }
+
+</script>
+
+<style scoped>
+</style>
